ab3737212d082ed274b75673f5efc6916b32f0db
beatrice-sorio <73838176+beatrice-sorio@users.noreply.github.com>
2024-05-21 19:21:53 +0200

    Add files via upload

--- sitorobot.js
+++ sitorobot.js
@@ -2,29 +2,28 @@ console.log(document);
 
 if (!!window.EventSource) {
   var source = new EventSource('/events');
-  
-  source.addEventListener('open', function(e) {
-   console.log("Events Connected");
-  }, false);
-  source.addEventListener('error', function(e) {
-   if (e.target.readyState != EventSource.OPEN) {
-     console.log("Events Disconnected");
-   }
-  }, false);
-  
-  source.addEventListener('message', function(e) {
-   console.log("message", e.data);
-  }, false);
- }
- 
-window.onload = ()=>{
-  console.log('va');
-  let canvas = document.createElement('canvas');
-  let ctx = canvas.getContext('2d');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  canvas.style.display = 'block';
-  document.body.appendChild(canvas);
 
-  document.body.style.margin = 0;
+  source.addEventListener('open', function (e) {
+    console.log("Events Connected");
+  }, false);
+  source.addEventListener('error', function (e) {
+    if (e.target.readyState != EventSource.OPEN) {
+      console.log("Events Disconnected");
+    }
+  }, false);
+
+  source.addEventListener('message', function (e) {
+    console.log("message", e.data);
+  }, false);
 }
+
+
+console.log('va');
+let canvas = document.createElement('canvas');
+let ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style.display = 'block';
+document.body.appendChild(canvas);
+
+document.body.style.margin = 0;
