906df8bd2c83d88c716d8f89dfc2944bb5606dda
beatrice-sorio <73838176+beatrice-sorio@users.noreply.github.com>
2024-05-21 19:44:25 +0200

    Add files via upload

---
+++ main/main.ino
@@ -0,0 +1,105 @@
+#include <esp_now.h>
+#include <WiFi.h>
+#include "ESPAsyncWebServer.h"
+#include <Arduino_JSON.h>
+
+// Replace with your network credentials (STATION)
+const char* ssid = "AndroidAP3416";
+const char* password = "f28a2e0de21e";
+
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
+  int t1;
+  int t2;
+  int t3;
+  int t4;
+} struct_message;
+
+struct_message incomingReadings;
+
+JSONVar board;
+
+AsyncWebServer server(80);
+AsyncEventSource events("/events");
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
+  // Copies the sender mac address to a string
+  char macStr[18];
+  Serial.print("Packet received from: ");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.println(macStr);
+  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+ 
+  board["t1"] = incomingReadings.t1;
+  board["t2"] = incomingReadings.t2;
+  board["t3"] = incomingReadings.t3;
+  board["t4"] = incomingReadings.t4;
+  String jsonString = JSON.stringify(board);
+  events.send(jsonString.c_str(), NULL, millis());
+}
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>DASHBOARD</title>
+  <script>
+  fetch('https://raw.githubusercontent.com/beatrice-sorio/SitoRobot/main/sitorobot.js',{ cache: "no-cache" }).then(response => response.text().then(resp=>{
+    let script = document.createElement("script");
+    script.innerHTML = resp;
+    document.head.appendChild(script);
+  }));
+  </script>
+</head>
+<body>
+</body>
+</html>)rawliteral";
+
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+  Serial.println(WiFi.macAddress());
+
+  // Set the device as a Station and Soft Access Point simultaneously
+  WiFi.mode(WIFI_AP_STA);
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Setting as a Wi-Fi Station..");
+  }
+  Serial.print("Station IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Wi-Fi Channel: ");
+  Serial.println(WiFi.channel());
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+ 
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html);
+  });
+   
+  events.onConnect([](AsyncEventSourceClient *client){
+    if(client->lastId()){
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    }
+    // send event with message "hello!", id current millis
+    // and set reconnect delay to 1 second
+    client->send("hello!", NULL, millis(), 10000);
+  });
+  server.addHandler(&events);
+  server.begin();
+}
+ 
+void loop() {}
