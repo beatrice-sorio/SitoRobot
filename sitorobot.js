2a4cdc4d72000e41586f52e087fe20da6d6fa4d6
beatrice-sorio <73838176+beatrice-sorio@users.noreply.github.com>
2024-05-21 18:50:22 +0200

    Add files via upload

--- sitorobot.js
+++ sitorobot.js
@@ -1,1 +1,18 @@
 console.log("CIAO MONDO");
+if (!!window.EventSource) {
+ var source = new EventSource('/events');
+ 
+ source.addEventListener('open', function(e) {
+  console.log("Events Connected");
+ }, false);
+ source.addEventListener('error', function(e) {
+  if (e.target.readyState != EventSource.OPEN) {
+    console.log("Events Disconnected");
+  }
+ }, false);
+ 
+ source.addEventListener('message', function(e) {
+    document.innerHTML+=e.data+'\n';
+  console.log("message", e.data);
+ }, false);
+}
