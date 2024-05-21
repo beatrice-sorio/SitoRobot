7054c00cb27ed650fcf0d7b680c47ae3eb2cbdff
beatrice-sorio <73838176+beatrice-sorio@users.noreply.github.com>
2024-05-21 19:08:26 +0200

    Add files via upload

--- sitorobot.js
+++ sitorobot.js
@@ -1,18 +1,30 @@
 console.log(document);
-if (!!window.EventSource) {
- var source = new EventSource('/events');
- 
- source.addEventListener('open', function(e) {
-  console.log("Events Connected");
- }, false);
- source.addEventListener('error', function(e) {
-  if (e.target.readyState != EventSource.OPEN) {
-    console.log("Events Disconnected");
-  }
- }, false);
- 
- source.addEventListener('message', function(e) {
-    document.innerHTML+=e.data+'\n';
-  console.log("message", e.data);
- }, false);
-}
+
+window.addEventListener("DOMContentLoaded",()=>{
+
+  let canvas = document.createElement('canvas');
+  let ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  canvas.style.display = 'block';
+  document.appendChild(canvas);
+
+  document.body.style.margin = 0;
+
+  if (!!window.EventSource) {
+    var source = new EventSource('/events');
+    
+    source.addEventListener('open', function(e) {
+     console.log("Events Connected");
+    }, false);
+    source.addEventListener('error', function(e) {
+     if (e.target.readyState != EventSource.OPEN) {
+       console.log("Events Disconnected");
+     }
+    }, false);
+    
+    source.addEventListener('message', function(e) {
+     console.log("message", e.data);
+    }, false);
+   }
+})
