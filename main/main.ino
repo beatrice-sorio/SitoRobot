93dcb1bbb99cc344d011eebceffb832f186d7cab
beatrice-sorio <73838176+beatrice-sorio@users.noreply.github.com>
2024-06-03 21:42:05 +0200

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -82,9 +82,9 @@ void setup() {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
- 
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
+
+  //Una volta inizializzato ESP-NOW, l'evento recv CB viene associato alla funzione OnDataRecv 
+  //per avere lo stato del pacchetto ricevuto
   esp_now_register_recv_cb(OnDataRecv);
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -95,8 +95,7 @@ void setup() {
     if(client->lastId()){
       Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
     }
-    // send event with message "hello!", id current millis
-    // and set reconnect delay to 1 second
+    // Invio di un pacchetto con il messaggio "hello!"
     client->send("hello!", NULL, millis(), 10000);
   });
   server.addHandler(&events);
